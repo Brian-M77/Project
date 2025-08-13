9ad1bfa56b0c17ed87d69a011eb359a88bc7dfe9
Brian-M77 <cm.briang@gmail.com>
2025-08-13 12:58:27 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let scoreH = document.getElementById("scoreH")
+let scoreG = document.getElementById("scoreG")
+let countH = 0
+function plus1(){
+    countH+=1   
+    scoreH.textContent = countH
+}
+function plus2(){
+    countH+=2    
+    scoreH.textContent = countH
+}
+function plus3(){
+    countH+=3  
+    scoreH.textContent = countH
+}
+let  countG=0
+
+function plus11(){
+    countG+=1    
+    scoreG.textContent = countG
+}
+function plus12(){
+    countG+=2    
+    scoreG.textContent = countG
+}
+function plus13(){
+    countG+=3   
+    scoreG.textContent = countG
+}
